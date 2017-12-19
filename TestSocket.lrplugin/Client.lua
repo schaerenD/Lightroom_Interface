f6ce25301e098faedd32f3700f48bf1ccf794a96
schaerenD <daniel.schaeren@gmail.com>
2017-12-19 17:12:37 +0100

    Revert "Revert "Revert "Funktionsfähig"""
    This reverts commit 9e73bc07e7ca5a680947d46e4c6fa44e35d7230d.

--- TestSocket.lrplugin/Client.lua
+++ TestSocket.lrplugin/Client.lua
@@ -15,7 +15,6 @@ BefehlString  	= "NIL"
 WertChar 		= 0 
 
 LastView		="loupe"
-KillAllTask 	= false
 
 Contrast		= 0
 Temp			= 0
@@ -53,86 +52,7 @@ SatMagenta		= 0
 LumMagenta		= 0
 Vignet 			= 0
 
-LimitsLow	= {}
-LimitsLow["Standard"]						= -100
-LimitsLow["Temperatur"]						= 2000
-LimitsLow["Exposure"]						= -500
-LimitsLow["Tint"]							= -150
-LimitsLow["Contrast"]						= -100
-LimitsLow["Shadows"]						= -100
-LimitsLow["Whites"]							= -100
-LimitsLow["Blacks"]							= -100
-LimitsLow["Lowlights"]						= -100
-LimitsLow["Clarity"]						= -100
-LimitsLow["Vibrance"]						= -100
-LimitsLow["Saturation"]						= -100
-LimitsLow["HueAdjustmentRed"]				= -100
-LimitsLow["SaturationAdjustmentRed"]		= -100
-LimitsLow["LuminanceAdjustmentRed"]			= -100
-LimitsLow["HueAdjustmentOrange"]			= -100
-LimitsLow["SaturationAdjustmentOrange"]		= -100
-LimitsLow["LuminanceAdjustmentOrange"]		= -100
-LimitsLow["HueAdjustmentYellow"]			= -100
-LimitsLow["SaturationAdjustmentYellow"]		= -100
-LimitsLow["LuminanceAdjustmentYellow"]		= -100
-LimitsLow["HueAdjustmentGreen"]				= -100
-LimitsLow["SaturationAdjustmentGreen"]		= -100
-LimitsLow["LuminanceAdjustmentGreen"]		= -100
-LimitsLow["HueAdjustmentAqua"]				= -100
-LimitsLow["SaturationAdjustmentAqua"]		= -100
-LimitsLow["LuminanceAdjustmentAqua"]		= -100
-LimitsLow["HueAdjustmentBlue"]				= -100
-LimitsLow["SaturationAdjustmentBlue"]		= -100
-LimitsLow["LuminanceAdjustmentBlue"]		= -100
-LimitsLow["HueAdjustmentPurple"]			= -100
-LimitsLow["SaturationAdjustmentPurple"]		= -100
-LimitsLow["LuminanceAdjustmentPurple"]		= -100
-LimitsLow["HueAdjustmentMagenta"]			= -100
-LimitsLow["SaturationAdjustmentMagenta"]	= -100
-LimitsLow["LuminanceAdjustmentMagenta"]		= -100
-LimitsLow["straightenAngle"]				= -100
-LimitsLow["PostCropVignetteAmount"]			= -100
-
-
-LimitsHigh	= {}
-LimitsHigh["Standard"] 						= 100
-LimitsHigh["Temperatur"]					= 50000
-LimitsHigh["Exposure"]						= 500
-LimitsHigh["Tint"]							= 150
-LimitsHigh["Contrast"]						= 100
-LimitsHigh["Shadows"]						= 100
-LimitsHigh["Whites"]						= 100
-LimitsHigh["Blacks"]						= 100
-LimitsHigh["Highlights"]					= 100
-LimitsHigh["Clarity"]						= 100
-LimitsHigh["Vibrance"]						= 100
-LimitsHigh["Saturation"]					= 100
-LimitsHigh["HueAdjustmentRed"]				= 100
-LimitsHigh["SaturationAdjustmentRed"]		= 100
-LimitsHigh["LuminanceAdjustmentRed"]		= 100
-LimitsHigh["HueAdjustmentOrange"]			= 100
-LimitsHigh["SaturationAdjustmentOrange"]	= 100
-LimitsHigh["LuminanceAdjustmentOrange"]		= 100
-LimitsHigh["HueAdjustmentYellow"]			= 100
-LimitsHigh["SaturationAdjustmentYellow"]	= 100
-LimitsHigh["LuminanceAdjustmentYellow"]		= 100
-LimitsHigh["HueAdjustmentGreen"]			= 100
-LimitsHigh["SaturationAdjustmentGreen"]		= 100
-LimitsHigh["LuminanceAdjustmentGreen"]		= 100
-LimitsHigh["HueAdjustmentAqua"]				= 100
-LimitsHigh["SaturationAdjustmentAqua"]		= 100
-LimitsHigh["LuminanceAdjustmentAqua"]		= 100
-LimitsHigh["HueAdjustmentBlue"]				= 100
-LimitsHigh["SaturationAdjustmentBlue"]		= 100
-LimitsHigh["LuminanceAdjustmentBlue"]		= 100
-LimitsHigh["HueAdjustmentPurple"]			= 100
-LimitsHigh["SaturationAdjustmentPurple"]	= 100
-LimitsHigh["LuminanceAdjustmentPurple"]		= 100
-LimitsHigh["HueAdjustmentMagenta"]			= 100
-LimitsHigh["SaturationAdjustmentMagenta"]	= 100
-LimitsHigh["LuminanceAdjustmentMagenta"]	= 100
-LimitsHigh["straightenAngle"]				= 100
-LimitsHigh["PostCropVignetteAmount"]		= 100
+KillAllTask = false
 ----------------- Global Variabel  ----------------
 ---------------- StartAsync Function --------------
 function StartAsync()
@@ -212,6 +132,7 @@ function AsyncTest(context)
 end
 	
 ---------------- AsyncTest Function ---------------
+
 --------------- BlackWhite Function ---------------
 function BlackWhite()
 
@@ -240,31 +161,22 @@ end
 --------------- BevorAfter Function ---------------
 
 function BevorAfter()
-
-	if 		LastView == "loupe" then LastView = "develop_before";
-	elseif 	LastView == "develop_before" then LastView = "develop_before_after_horiz"
-	elseif 	LastView == "develop_before_after_horiz" then LastView = "develop_before_after_vert"
-	elseif 	LastView == "develop_before_after_vert" then LastView = "grid"
-	elseif 	LastView == "grid" then LastView = "compare";
-	elseif 	LastView == "compare" then LastView = "survey";
-	elseif 	LastView == "survey" then LastView = "loupe";
-	else	LastView =  "loupe"
+	
+	LrApplicationView.showView("loupe")
+	if 		LastView == "develop_before" then LastView = "develop_before"
+	elseif 	LastView == "develop_before_after_horiz" then LastView = "develop_before_after_horiz"
+	elseif 	LastView == "develop_before_after_vert" then LastView = "develop_before_after_vert"
+	elseif 	LastView == "grid" then LastView = "grid";
+	elseif 	LastView == "compare" then LastView = "compare";
+	elseif 	LastView == "survey" then LastView = "survey";
+	else
 	end
-	LrApplicationView.showView(LastView)
+	
+
 
 end
 		
 	
---------------- BevorAfter Function --------------
------------- BevorAfterReset Function ------------
-
-function BevorAfterReset()
-
-		LastView = "loupe";
-		LrApplicationView.showView(LastView)
-	
-end
-		
 --------------- BevorAfter Function --------------
 ------------------- Up Function ------------------
 
@@ -454,27 +366,17 @@ end
 function ChangeExposure(Groesse,Richtung)
 	
 		local valueDevelopt = 0
-		
+	
 		LrApplicationView.switchToModule('develop')		-- Change Module
 		LrDevelopController.revealPanel(Groesse)		-- Pop Up Menü
 	
 		valueDevelopt = LrDevelopController.getValue(Groesse);
 		valueDevelopt = 100*valueDevelopt;
-
-
-
 		if     	Richtung == "positiv" then valueDevelopt = valueDevelopt + WertChar;                             
 		elseif 	Richtung == "negativ" then valueDevelopt = valueDevelopt - WertChar;
 		elseif	Richtung == "zero"	  then valueDevelopt = 0;
 		else	LrDialogs.message( "Das hat nicht geklappt", "ChangeAdjustPanel", "info" )
 		end
-
-		-- Begrenzungen
-		if valueDevelopt > LimitsHigh[Groesse] then valueDevelopt = LimitsHigh[LimitsHigh]
-		elseif valueDevelopt < LimitsLow[Groesse] then valueDevelopt = LimitsLow[Groesse] 
-		else valueDevelopt = valueDevelopt
-		end
-
 		valueDevelopt = valueDevelopt/100;	   
 		LrDevelopController.setValue(Groesse,valueDevelopt);
 	
@@ -498,13 +400,6 @@ function ChangeAdjustPanel(Groesse,Richtung)
 	elseif	Richtung == "zero"	  then valueDevelopt = 0;
 	else	LrDialogs.message( "Das hat nicht geklappt", "ChangeAdjustPanel", "info" )
 	end	   
-
-	-- Begrenzungen
-	if valueDevelopt > LimitsHigh["Standard"] then valueDevelopt = LimitsHigh["Standard"]
-	elseif valueDevelopt < LimitsLow["Standard"] then valueDevelopt = LimitsLow["Standard"] 
-	else valueDevelopt = valueDevelopt
-	end
-
 	LrDevelopController.setValue(Groesse,valueDevelopt);
 
 end
@@ -654,8 +549,6 @@ function ExecuteFunction()
 	elseif BefehlString == "Zoom"		then LrApplicationView.toggleZoom()
 	elseif BefehlString == "Full"		then switchToFullscreen()
 	elseif BefehlString == "Bevor"		then BevorAfter()
-	elseif BefehlString == "BevorReset"	then BevorAfterReset()
-
 	elseif BefehlString == "Grid" 		then LrApplicationView.switchToModule('library')
 	elseif BefehlString == "Devel"		then LrApplicationView.switchToModule('develop')
 
@@ -765,7 +658,7 @@ LrTasks.startAsyncTask(
 				
 				--Funktion OnConnect()
 				onConnected = function( socket, port ) 
-					LrDialogs.message( "Connection established Vers.:127", "4242", "info" )
+					LrDialogs.message( "Connection established Vers.:120	", "4242", "info" )
 				end,
 				
 				--Funktion OnMessage()
